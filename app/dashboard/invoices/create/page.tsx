a951aaefa3f26f9b71ec4ed73096aa4cbdab2367
AlexTey99 <teyalex99@gmail.com>
2024-10-14 12:51:43 +0200

    Adicion de fomrulario

--- app/dashboard/invoices/create/page.tsx
+++ app/dashboard/invoices/create/page.tsx
@@ -1,7 +1,19 @@
-export default function CreateInvoice () {
-    return(
-        <>
-        <h1>Create Invoice</h1>
-        </>
-    )
+import Form from '@/app/ui/invoices/create-form';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import { fetchCustomers } from '@/app/lib/data';
+ 
+export default async function CreateInvoice() {
+  const customers = await fetchCustomers();
+ 
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Invoices', href: '/dashboard/invoices' },
+          { label: 'Create Invoice', href: '/dashboard/invoices/create', active: true,},
+        ]}
+      />
+      <Form customers={customers} />    
+    </main>
+  );
 }
--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -3,6 +3,7 @@ import Search from "@/app/ui/search";
 import Pagination from '@/app/ui/invoices/pagination';
 import { CreateInvoice } from "@/app/ui/invoices/buttons";
 import { lusitana } from '@/app/ui/fonts';
+import { fetchInvoicesPages } from '@/app/lib/data';
 
 
 export default async function InvoicesPage({
@@ -15,6 +16,8 @@ export default async function InvoicesPage({
   }) {
     const query = searchParams?.query || '';
     const currentPage = Number(searchParams?.page) || 1;
+
+    const totalPages = await fetchInvoicesPages(query);
     return (
         <> 
             <h1 className={`${lusitana.className} mb-8 text-xl md:text-2xl`}>
@@ -27,7 +30,7 @@ export default async function InvoicesPage({
          
             <InvoicesTable query={query} currentPage={currentPage} />
             <div className="mt-5 flex w-full justify-center">
-                <Pagination totalPages={9} />
+                <Pagination totalPages={totalPages} />
             </div>
         </>
     )
